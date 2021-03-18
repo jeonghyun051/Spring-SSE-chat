3dea39f8efbf9fc282b1dccbf48b2e52032a598e
kimjeonghyun <rlawjdgus051@naver.com>
2021-03-18 15:01:27 +0900

    spring sse 채팅

---
+++ rsschat/src/main/java/com/cos/rsschat/RsschatApplication.java
@@ -0,0 +1,13 @@
+package com.cos.rsschat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsschatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RsschatApplication.class, args);
+	}
+
+}
---
+++ rsschat/src/test/java/com/cos/rsschat/RsschatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cos.rsschat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RsschatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
